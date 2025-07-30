c166ab82d264ebd1a2da54235d353f7640012338
noobcoderRoshan <RK9055436@gmail.com>
2025-07-30 11:25:43 +0530

    passing Arguments

--- arrays.java
+++ arrays.java
@@ -1,15 +1,19 @@
 import java.util.*;
 
-public class arrays {
-    public static void main(String args[]) {
-        int marks[] = new int[100];
-        Scanner sc = new Scanner(System.in);
-        marks[0] = sc.nextInt();
-        marks[1] = sc.nextInt();
-        marks[2] = sc.nextInt();
-        System.out.println("phy: " + marks[0]);
-        System.out.println("chem: " + marks[1]);
-        System.out.println("math: " + marks[2]);
-        sc.close();
+public static void update(int marks[]) {
+    for (int i = 0; i < marks.length; i++) {
+        marks[i] = marks[i] + 1;
+
     }
+
+}
+
+public static void main(String args[]) {
+    int marks[] = { 80, 30, 90 };
+    update(marks);
+    for(int i = 0;i<marks.length;i++){
+        System.out.print(marks[i]+" ");
+    }
+    System.out.println();
+
 }
